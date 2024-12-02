e0fa56dee0a545404ad875d5d7464755905788ef
Charlie Eeles <charlie.eeles@gmail.com>
2024-12-02 23:59:51 +0000

    Clean up day 01 pt 01

--- day_02/src/bin/part_1.rs
+++ day_02/src/bin/part_1.rs
@@ -1,35 +1,25 @@
 fn main() {
     let input = include_str!("../input.txt");
 
-    //TODO: hah this is messy, needs cleaning up
-    let mut num_of_safe_reports: u32 = 0;
-    for line in input.lines() {
-        let mut prev_num: u32 = 0;
-        let mut is_unsafe = false;
-        let mut is_increasing = false;
+    let num_of_safe_reports = input
+        .lines()
+        .filter(|line| {
+            let report_vec: Vec<u32> = line
+                .split_whitespace()
+                .map(|str_num| str_num.parse::<u32>().unwrap())
+                .collect();
+            let is_ascending = report_vec[0] < report_vec[1];
 
-        line.split_whitespace()
-            .enumerate()
-            .for_each(|(idx, str_num)| {
-                let num: u32 = str_num.parse().unwrap();
-                if idx > 0 && !is_unsafe {
-                    let max = num.max(prev_num);
-                    let min = num.min(prev_num);
-                    let mut out_of_order = false;
+            !report_vec.windows(2).any(|num| {
+                let max = num[0].max(num[1]);
+                let min = num[0].min(num[1]);
+                let diff = max - min;
 
-                    if idx == 1 {
-                        is_increasing = max == num;
-                    } else if is_increasing && max != num || !is_increasing && min != num {
-                        out_of_order = true;
-                    }
-                    let diff = max - min;
-                    is_unsafe = !(1..=3).contains(&diff) || out_of_order;
-                }
-                prev_num = num;
-            });
-        if !is_unsafe {
-            num_of_safe_reports += 1;
-        }
-    }
+                !(1..=3).contains(&diff)
+                    || (is_ascending && max == num[0])
+                    || (!is_ascending && min == num[0])
+            })
+        })
+        .count();
     println!("Num of safe reports: {num_of_safe_reports}");
 }
